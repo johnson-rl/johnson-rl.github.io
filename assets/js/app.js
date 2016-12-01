7370689bc1c48f60ca74baf00808244019fd9379
johnson-rl <johnson.rl@gmail.com>
2016-11-30 19:52:08 -0800

    first draft of web page, desperately needs better color scheme.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,26 @@
 // sanity check
 console.log("app.js linked.");
+
+var elementEdit = document.getElementById("most-recent");
+
+function printPyramid(length){
+    var x = ''
+    for (j = 0;j<length-1;j++){
+        x+=' '
+    }
+    // var element = document.getElementsByClassName('most-recent')
+    for (var i = 0; i<length; i++){
+        x = x.slice(1)
+        x += ' *';
+        var node = document.createElement('div');
+        var textNode = document.createTextNode(x);
+        node.appendChild(textNode);
+        console.log(node);
+        console.log(elementEdit);
+        // elementEdit += node
+        elementEdit.appendChild(node);
+        console.log(x);
+    }
+}
+
+printPyramid(10)
